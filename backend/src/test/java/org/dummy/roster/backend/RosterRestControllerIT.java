eaa7ed482e82f8f2cd089ad4b6a9ac69c6d64e80
Anatoly Shipov <anatoly.a.shipov@gmail.com>
2019-12-08 11:41:39 +0300

    msg

--- backend/src/test/java/org/dummy/roster/backend/RosterRestControllerIT.java
+++ backend/src/test/java/org/dummy/roster/backend/RosterRestControllerIT.java
@@ -1,26 +1,30 @@
 package org.dummy.roster.backend;
 
+import org.dummy.roster.backend.repository.EmployeeRepository;
+import org.dummy.roster.backend.repository.SalaryRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.dummy.roster.backend.rest.RosterRestController;
-import org.dummy.roster.backend.service.RosterService;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import static org.dummy.roster.backend.RestUtils.buildPost;
 import static org.dummy.roster.backend.TestUtils.makeADummy;
 import static org.dummy.roster.backend.rest.RosterRestController.ROSTER_PATH;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
- * Тест {@link RosterRestController}.
+ * Integration test {@link org.dummy.roster.backend.rest.RosterRestController}.
  */
 @RunWith(SpringRunner.class)
-@WebMvcTest(RosterRestController.class)
-public class RosterRestControllerTest {
+@SpringBootTest()
+@AutoConfigureMockMvc
+@TestPropertySource(locations = "classpath:integration-test.properties")
+public class RosterRestControllerIT {
 
     /**
      * {@link MockMvc}.
@@ -29,10 +33,16 @@ public class RosterRestControllerTest {
     private MockMvc mvc;
 
     /**
-     * {@link RosterService}.
+     * {@link EmployeeRepository}.
      */
-    @MockBean
-    private RosterService rosterService;
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    /**
+     * {@link SalaryRepository}.
+     */
+    @Autowired
+    private SalaryRepository salaryRepository;
 
     @Test
     public void saveTest() {
