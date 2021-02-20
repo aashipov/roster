2ee656c39faca5023f1568a2d3a387ebcaccad38
Anatoly Shipov <anatoly.a.shipov@gmail.com>
2021-02-20 04:08:45 +0300

    msg

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -6,30 +6,22 @@ const EMPLOYEES = `/employees`;
 const BACKEND = `${process.env.NODE_ENV !== "production" ? DEFAULT_LOCALHOST : window.location.origin}`;
 const API_V1_EMPLOYEES = BACKEND + API_V1 + EMPLOYEES;
 
-export function readAllEmployees() {
-    return axios.get(API_V1_EMPLOYEES).then(resp => {
-        return resp.data
-    });
-}
+export const readAllEmployees = () =>
+    axios
+        .get(API_V1_EMPLOYEES)
+        .then((resp) => resp.data);
 
-export function createEmployee(employee) {
-    return axios.post(API_V1_EMPLOYEES, employee).then(resp => {
-        return resp.data
-    });
-}
+export const createEmployee = (employee) =>
+    axios
+        .post(API_V1_EMPLOYEES, employee)
+        .then((resp) => resp.data);
 
-export function updateEmployee(employee) {
-    return axios.put(API_V1_EMPLOYEES + `/` + employee.id, employee).then(resp => {
-        return resp.data
-    });
-}
+export const updateEmployee = (employee) =>
+    axios
+        .put(API_V1_EMPLOYEES + `/` + employee.id, employee)
+        .then((resp) => resp.data);
 
-//Fails in node.js
-export function deleteAll(user, password) {
-    return axios
+export const deleteAll = (user, password) =>
+    axios
         .delete(API_V1_EMPLOYEES, {headers: {Authorization: "Basic " + btoa(user + ":" + password)}})
-        .then(resp => {
-            return resp
-        });
-}
-
+        .then((resp) => resp);
