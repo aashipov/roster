44db25e864c7c9fdf50fa52271576bd0c13bde88
Anatoly Shipov <anatoly.a.shipov@gmail.com>
2021-02-23 07:11:44 +0300

    msg

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,26 +1,27 @@
 import {ADD_EMPLOYEE, DELETE_ALL, GET_ALL_EMPLOYEES, UDATE_EMPLOYEE} from '../constants';
 import {combineReducers} from "redux";
 
-// NOSONAR
-const employees = (state = [], action) => {
+const employees = (state = [], action) => { //nosonar
     switch (action.type) {
-        case GET_ALL_EMPLOYEES : {
+        case GET_ALL_EMPLOYEES: {
             return Object.assign([], state, action.employees);
         }
-        case ADD_EMPLOYEE :
+        case ADD_EMPLOYEE:
             return [
                 ...state,
                 action.employee
             ];
         case UDATE_EMPLOYEE: {
-            state.map((emp, idx) => {
-                if (emp.id === action.employee.id) {
-                    Object.assign({}, emp, action.employee)
+            state.map(
+                (emp, idx) => {
+                    if (emp.id === action.employee.id) {
+                        Object.assign({}, emp, action.employee)
+                    }
                 }
-            });
+            );
             return state;
         }
-        case DELETE_ALL : {
+        case DELETE_ALL: {
             return [];
         }
         default:
